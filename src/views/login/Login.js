c9e0af152d3fb5401e7995448f830f220131aeb0
RawatTushar <tusharrawatdpss@gmail.com>
2026-07-10 09:50:48 +0530

    check the changes

--- src/views/login/Login.js
+++ src/views/login/Login.js
@@ -59,7 +59,7 @@ const Login = ({ navigation }) => {
       source={{ uri: 'https://via.placeholder.com/800x1400' }}>
       <KeyboardAwareScrollView contentContainerStyle={styles.wrapper}>
         <View style={styles.form}>
-          <Text style={styles.title}>MobiHISTree login</Text>
+          <Text style={styles.title}>MOBIHISTREE LOGIN</Text>
           <LoginIdPasswordContainer
             placeholder="UserID"
             value={username}
